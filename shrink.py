4b4a28fcd6fc782ce26040cfab0570e0dae87fc4
Bonney Omondi <ochiengbonn2018@gmail.com>
2026-04-26 11:39:34 +0300

    Remove unnecessary whitespace and improve formatting

--- shrink.py
+++ shrink.py
@@ -1,9 +1,9 @@
 import joblib
 
-# 1. Load your original model (only once, locally)
+# 1. Load your original model
 model = joblib.load('model.pkl')
 
 # 2. Save it using joblib with compression
 joblib.dump(model, 'car_price_model.joblib', compress=3)
 
-print("✅ Model successfully compressed with joblib!")
+print("Model successfully compressed with joblib!")
